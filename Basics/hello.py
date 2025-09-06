75d318ca2c617c85f129a54f7c5c28a44dbf726d
AayushKThakur <connecttoakt@gmail.com>
2025-09-06 10:03:51 +0530

    initial commit

---
+++ Basics/hello.py
@@ -0,0 +1,10 @@
+print('hello world!')
+print("This is AKT")
+
+# end parameter in print()
+# to print subsequent print statements in one line. 
+# which allows you to specify a different string (such as a space, comma, or hyphen)
+# to be printed at the end, enabling multiple outputs to appear on the same line or with custom separators.
+print('My name is Aayush Kunal Thakur.', end = ' ')
+print('I am 22 years old', end = ' next month. ')
+print('My gender is Male', end = ' ')
